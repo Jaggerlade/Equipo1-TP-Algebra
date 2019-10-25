d0204db5e4dfcfb1cc39c21dba2a2653fc149531
Ignacio Otegui <nachootegui@hotmail.com>
2019-10-24 21:55:48 -0300

    SetScene
    Ahora en la escena hay una mesa pool y una pelota que se mueve cuando haces un click. También intenté hacer que el palo de pool siguiera a mi mouse, pero no lo logré

---
+++ PoolTeam1/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Translate(Vector2.up * speed* Time.deltaTime);
+        }
+    }
+}
---
+++ PoolTeam1/Assets/Scripts/Sticks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sticks : MonoBehaviour
+{
+    public Camera cam;
+    Vector2 mousePos;
+    
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
