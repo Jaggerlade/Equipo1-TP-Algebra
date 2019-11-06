d9a6e3ffdf9274973239023b21c5c611f0f29ce9
Juan Cruz Tourret <jctourret@gmail.com>
2019-11-06 13:22:55 -0300

    Agregado CollisionManager

---
+++ PoolTeam1/Assets/Scripts/Balls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    public float ratio;
+    [HideInInspector] public BallMovement ballMovement;
+    void Start()
+    {
+        ballMovement = gameObject.AddComponent<BallMovement>(); 
+        CollisionManager.instance.balls.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ratio);
+    }
+}
---
+++ PoolTeam1/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    public static CollisionManager instance;
+    public List<Balls> balls = new List<Balls>();
+    void Awake()
+    {
+        instance = this;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < balls.Count; i++)
+        {
+            for (int j = 0; j < balls.Count; j++)
+            {
+                if (balls[i] = balls[j])
+                {
+                    return;
+                }
+                else
+                {
+                    /*if (balls[i].ballMovement.transform.position + balls[j].ballMovement.transform.position
+                        <= )
+                    {
+
+                    }*/
+                }
+            }
+        }
+       
+
+     
+    }
+}
