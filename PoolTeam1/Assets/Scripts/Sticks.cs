03050ef570ffefd71e17b9e990698b82d034f118
Juan Cruz Tourret <jctourret@gmail.com>
2019-11-07 17:48:12 -0300

    Correcciónes menores de Assets

--- PoolTeam1/Assets/Scripts/Sticks.cs
+++ PoolTeam1/Assets/Scripts/Sticks.cs
@@ -6,18 +6,19 @@ public class Sticks : MonoBehaviour
 {
     public Camera cam;
     Vector2 mousePos;
-    
+    private float Offset = 0;
     
 
     void Start()
     {
-        
+        transform.position = new Vector3(transform.position.x, transform.position.y, Offset);
     }
 
     // Update is called once per frame
     void Update()
     {
         mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = (mousePos);
         //transform.position = (mousePos);
         
 
