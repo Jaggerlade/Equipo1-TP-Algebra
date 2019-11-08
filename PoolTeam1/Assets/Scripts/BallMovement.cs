5f8b89c8f48a6bcbd757f27c77e978d3956f565a
Juan Cruz Tourret <jctourret@gmail.com>
2019-11-07 23:43:24 -0300

    Comentada posible funcion de colisiones

--- PoolTeam1/Assets/Scripts/BallMovement.cs
+++ PoolTeam1/Assets/Scripts/BallMovement.cs
@@ -12,43 +12,40 @@ public class BallMovement : MonoBehaviour
     float aceleration;
     float force;
     float time;
-
+    /*
+    float radius;
+    Vector3 Speed;
+    Vector3 mouseClickPoint;
+    Vector3 ballDirection;
+    */
     public float radius;
 
-    
-
-    void Start()
-    {
-        
+    void Start(){
         aceleration = 0.0f;
         force = 0.0f;
         time = 0.0f;
     }
-
-    void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
+    void FixedUpdate(){
+        if (Input.GetKey(KeyCode.Mouse0)){
             time += Time.deltaTime * 3;
+            /*
+            La acumulacion no deberia ser infinita. Tal vez poner if(time<=10.0)?
+            Para poder hacer que la direccion de la pelota sea en base a donde se clickeo:
+            mouseClickPoint=mouse.transform.position;
+            ballDirection=ballPosition-mouseClickPoint;
+            */
         }
-        
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
+        if (Input.GetKeyUp(KeyCode.Mouse0)){
             aceleration = time;
             force = mass * aceleration;
             time = 0.0f;
         }
-       
-        if (force >= 0.0f)
-        {
-            transform.Translate(Vector2.up * force * Time.deltaTime);
-
+        if (force >= 0.0f){
+            transform.Translate(Vector2.up /*Sacar Vector2.up y poner ballDirection*/ * force * Time.deltaTime);
             force -= CalculateFriction();
         }
     }
-
-    float CalculateFriction()
-    {
+    float CalculateFriction(){
         float Fr = 0.0f;
         float NormalF = mass * gravity;
 
@@ -56,8 +53,7 @@ public class BallMovement : MonoBehaviour
 
         return Fr;
     }
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos(){
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
--- PoolTeam1/Assets/Scripts/CollisionManager.cs
+++ PoolTeam1/Assets/Scripts/CollisionManager.cs
@@ -25,12 +25,19 @@ public class CollisionManager : MonoBehaviour
                     return;
                 }
                 else
-                {
-                    /*if (balls[i].ballMovement.transform.position + balls[j].ballMovement.transform.position
-                        <= )
-                    {
-
-                    }*/
+                {/*
+                    if (balls[i].ballMovement.transform.position + balls[j].ballMovement.transform.position
+                        <= balls[i].radius+balls[j].radius){
+                        balls[j].Speed=balls[j].transformation.position-balls[i].transformation.position;
+                        float prodEscalar = (balls[i].Speed.x+balls[j].Speed.x)*(balls[i].Speed.y+balls[j].Speed.y);
+                        float angulo1 = Mathf.Acos(prodEscalar/(Mathf.Sqrt((Mathf.Pow(balls[i].Speed.x,2)+(Mathf.Pow(balls[i].Speed.y,2)*(Mathf.Pow(balls[j].Speed.x,2)+(Mathf.Pow(balls[j].Speed.y,2));
+                        ball[j].Speed.x = (ball[j].Speed.x*Mathf.Cos(angulo1));
+                        ball[j].Speed.y = (ball[j].Speed.x*Mathf.Cos(angulo1));
+                        float angulo2 = 90-angulo1;
+                        ball[i].Speed.x = (ball[i].Speed.x*Mathf.Cos(angulo1));
+                        ball[i].Speed.y = (ball[i].Speed.x*Mathf.Cos(angulo1));
+                    }
+                */
                 }
             }
         }
