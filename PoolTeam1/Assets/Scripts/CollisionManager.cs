8b69e5babf7ebf2e0fd11b6195b1f2a6827b8696
Lucio Ruixo <lucioruixo@gmail.com>
2019-11-19 22:15:14 -0300

    Agregradas colisiones con paredes verticales

--- PoolTeam1/Assets/Scripts/CollisionManager.cs
+++ PoolTeam1/Assets/Scripts/CollisionManager.cs
@@ -25,26 +25,70 @@ public class CollisionManager : MonoBehaviour
     {
         for (int i = 0; i < balls.Count; i++)
         {
-            for (int j = 0; j < balls.Count; j++)
-            {
-                if (balls[i] != balls[j])
-                {
-                    if (Vector3.Distance(balls[i].ballMovement.transform.position, balls[j].ballMovement.transform.position)
-                        <= balls[i].ballMovement.radius + balls[j].ballMovement.radius)
-                    {
-                        //bola impactante
-                        balls[i].ballMovement.launchDirection = balls[i].transform.position - balls[j].transform.position;
-                        float prodEscalar = Vector3.Angle(balls[i].ballMovement.launchDirection, balls[j].ballMovement.launchDirection);
-                        balls[i].ballMovement.launchDirection = (balls[i].ballMovement.launchDirection * Mathf.Cos(prodEscalar));
+            ApplyBallsCollsions(i);
+            ApplyWallsCollsions(i);
 
-                        //bola impactada
-                        float impactingExitAngle = rightAngle - prodEscalar;
-                        balls[j].ballMovement.force = balls[j].ballMovement.force + balls[i].ballMovement.force;
-                        balls[j].ballMovement.launchDirection = (balls[i].ballMovement.launchDirection * Mathf.Cos(impactingExitAngle));
-                        //break;
-                        return;
-                    }
+            Debug.Log(balls[1].ballMovement.launchDirection);
+        }
+    }
+
+    void ApplyBallsCollsions(int i)
+    {
+        for (int j = 0; j < balls.Count; j++)
+        {
+            if (balls[i] != balls[j])
+            {
+                if (Vector3.Distance(balls[i].ballMovement.transform.position, balls[j].ballMovement.transform.position)
+                    <= balls[i].ballMovement.radius + balls[j].ballMovement.radius)
+                {
+                    //bola impactante
+                    balls[i].ballMovement.launchDirection = balls[i].transform.position - balls[j].transform.position;
+                    float prodEscalar = Vector3.Angle(balls[i].ballMovement.launchDirection, balls[j].ballMovement.launchDirection);
+                    balls[i].ballMovement.launchDirection = (balls[i].ballMovement.launchDirection * Mathf.Cos(prodEscalar));
+
+                    //bola impactada
+                    float impactingExitAngle = rightAngle - prodEscalar;
+                    balls[j].ballMovement.force = balls[j].ballMovement.force + balls[i].ballMovement.force;
+                    balls[j].ballMovement.launchDirection = (balls[i].ballMovement.launchDirection * Mathf.Cos(impactingExitAngle));
+                    //break;
+                    return;
                 }
+            }
+        }
+    }
+
+    void ApplyWallsCollsions(int i)
+    {
+        for (int j = 0; j < walls.Count; j++)
+        {
+            switch (walls[j].type)
+            {
+                case Walls.Type.Left:
+                    if (balls[i].transform.position.x + balls[i].ballMovement.radius >= walls[j].transform.position.x)
+                    {
+                        balls[i].ballMovement.launchDirection.x *= -1;
+                    }
+                    break;
+                case Walls.Type.Right:
+                    if (balls[i].transform.position.x - balls[i].ballMovement.radius <= walls[j].transform.position.x)
+                    {
+                        balls[i].ballMovement.launchDirection.x *= -1;
+                    }
+                    break;
+                case Walls.Type.Upper:
+                    if (balls[i].transform.position.y >= walls[j].transform.position.y)
+                    {
+                        balls[i].ballMovement.launchDirection.y *= -1;
+                        Debug.Log("Colision");
+                    }
+                    break;
+                case Walls.Type.Bottom:
+                    if (balls[i].transform.position.y <= walls[j].transform.position.y)
+                    {
+                        balls[i].ballMovement.launchDirection.y *= -1;
+                        Debug.Log("Colision");
+                    }
+                    break;
             }
         }
     }
